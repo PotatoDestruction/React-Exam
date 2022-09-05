7e9045e118f4f7aa604a0c2d072d29a0bbf7899e
ZilvProject <zzilvinas333@gmail.com>
2022-09-05 23:52:30 +0300

    finishing

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import './Layout.css'
 
+
+
 const Layout = ({ children, pages }) => {
+    
+
+    const [on, setOn] = useState(false)
+
+    useEffect(() => {
+        const isOnline = localStorage.getItem('tokenR')
+        setOn(isOnline)
+    })
     return (
         <div>
-            {console.log(pages)}
+            <div className="status">{on ? <div>Online <div className="logOut"><Link to='/login' >logout</Link></div></div> : 'Offline'}</div>
             <header>
                 <div><img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="logo" width='200px'/></div>
                 <div>
