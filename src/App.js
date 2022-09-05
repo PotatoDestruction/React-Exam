420b6005618bdfa5e12e604f9c702372875a037a
ZilvProject <zzilvinas333@gmail.com>
2022-09-05 18:46:19 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Main from './components/Main';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<div>Register</div>} />
+      <Route path='/login' element={<div>Login</div>} />
+      <Route path='/main' element={<div>main after login</div>} />
+      <Route path='/add' element={<div>add logged in</div>} />
+      <Route path='*' element={<div>No page</div>} />
+    </Routes>
+  );
+}
+
+export default App;
